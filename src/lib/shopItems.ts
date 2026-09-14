6628a56fdc5259d349d9b8a8547f0766a31ba309
mastercoder26 <akhilkonduru23@gmail.com>
2026-09-14 10:28:57 -0500

    Add Power Bank Grant to shop items

--- src/lib/shopItems.ts
+++ src/lib/shopItems.ts
@@ -39,6 +39,7 @@ export const allShopItems: ShopItem[] = [
   { name: "$70 MX Master 3S Grant", price: 12, img: "/prizes/mouse.png", description: "$70 for mx master 3s" },
   // ===== 17 HOURS =====
   { name: "YubiKey 5C NFC", price: 20, img: "/prizes/yubikey.jpg", description: "secure your shi" },
+  { name: "Power Bank Grant", price: 17, img: "/prizes/powerbank.png", description: "Stay juiced up" },
 
   // ===== 20 HOURS =====
   { name: "Keychron K3 QMK Wireless Mechanical Keyboard", price: 20, img: "/prizes/keychron_k3.jpg", description: "programmable mechanical keyboard" },
